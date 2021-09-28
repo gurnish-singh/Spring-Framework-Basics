20e06af0b0a726a468b036762b54a997c2dc6b64
Gurnish <gsingh11_be17@thapar.edu>
2021-09-28 16:41:19 +0530

    using multiple external properties

--- Spring course/UsingExternalProperties/src/main/java/com/gurnish/usingexternalproperties/UsingExternalPropertiesApplication.java
+++ Spring course/UsingExternalProperties/src/main/java/com/gurnish/usingexternalproperties/UsingExternalPropertiesApplication.java
@@ -1,11 +1,11 @@
 package com.gurnish.usingexternalproperties;
 
+import com.gurnish.usingexternalproperties.pojo.AbsFakedataModel;
+import com.gurnish.usingexternalproperties.pojo.FakeJmsdataModel;
 import com.gurnish.usingexternalproperties.pojo.FakedataModel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-
-import java.sql.SQLOutput;
 
 @SpringBootApplication
 public class UsingExternalPropertiesApplication {
@@ -13,11 +13,18 @@ public class UsingExternalPropertiesApplication {
     public static void main(String[] args) {
 
         ApplicationContext ctx= SpringApplication.run(UsingExternalPropertiesApplication.class, args);
-        FakedataModel fakedataModel= (FakedataModel) ctx.getBean(FakedataModel.class); //we are using configuration
-//        FakedataModel fakedataModel=(FakedataModel) ctx.getBean("fakedataModel") ;
+       FakedataModel fakedataModel = (FakedataModel) ctx.getBean(FakedataModel.class); //we are using configuration
+//        FakedataModel FakedataModel=(FakedataModel) ctx.getBean("fakedataModel") ;
         System.out.println(fakedataModel.getUser());
         System.out.println(fakedataModel.getPassword());
         System.out.println(fakedataModel.getUrl());
+
+        //tried using multiple external properties
+        FakeJmsdataModel fakeJmsdataModel=(FakeJmsdataModel) ctx.getBean("fakeJmsdataModel") ;
+        System.out.println(fakeJmsdataModel.getUser());
+        System.out.println(fakeJmsdataModel.getPassword());
+        System.out.println(fakeJmsdataModel.getUrl());
+
     }
 
 }
--- Spring course/UsingExternalProperties/src/main/java/com/gurnish/usingexternalproperties/configurations/ExternalPropertiesConfig.java
+++ Spring course/UsingExternalProperties/src/main/java/com/gurnish/usingexternalproperties/configurations/ExternalPropertiesConfig.java
@@ -1,29 +1,51 @@
 package com.gurnish.usingexternalproperties.configurations;
 
+import com.gurnish.usingexternalproperties.pojo.AbsFakedataModel;
+import com.gurnish.usingexternalproperties.pojo.FakeJmsdataModel;
 import com.gurnish.usingexternalproperties.pojo.FakedataModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
-@PropertySource("classpath:datasource.properties")
+@PropertySource({"classpath:datasource.properties","classpath:jms.properties"})
 public class ExternalPropertiesConfig {
+    @Autowired
+    Environment env;
     @Value("${gurnish.user}")
     String user;
     @Value("${gurnish.password}")
     String password;
     @Value("${gurnish.url}")
     String url;
+    @Value("${gurnish.jms.user}")
+    String jmsUsername;
+    @Value("${gurnish.jms.password}")
+    String jmsPassword;
+    @Value("${gurnish.jms.url}")
+    String jmsUrl;
     @Bean
     public FakedataModel fakedataModel(){
-        FakedataModel fakedataModel=new FakedataModel();
-        fakedataModel.setUser(user);
-        fakedataModel.setPassword(password);
-        fakedataModel.setUrl(url);
-        return fakedataModel;
+        FakedataModel FakedataModel =new FakedataModel();
+        FakedataModel.setUser(user);
+//        absFakedataModel.setUser(env.getProperty("USERNAME"));
+       FakedataModel.setPassword(password);
+        FakedataModel.setUrl(url);
+        return FakedataModel;
     }
+    @Bean  ////this doesnt work apparently because i dont know how to make multiple beans of same pojo
+    public FakeJmsdataModel fakeJmsdataModel(){
+        FakeJmsdataModel fakeJmsdataModel=new FakeJmsdataModel();
+        fakeJmsdataModel.setUser(jmsUsername);
+        fakeJmsdataModel.setPassword(jmsPassword);
+        fakeJmsdataModel.setUrl(jmsUsername);
+        return fakeJmsdataModel;
+    }
+
+
 //    @Bean//////this is what allows us to wire up our values
 //    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer(){
 //        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer=new PropertySourcesPlaceholderConfigurer();
