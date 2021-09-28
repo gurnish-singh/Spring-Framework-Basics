256ce67a0a0f80b09430abb26b2baf1dce751976
Gurnish <gsingh11_be17@thapar.edu>
2021-09-28 17:15:20 +0530

    using Yaml property file also found a bug

--- Spring course/UsingExternalProperties/src/main/java/com/gurnish/usingexternalproperties/configurations/ExternalPropertiesConfig.java
+++ Spring course/UsingExternalProperties/src/main/java/com/gurnish/usingexternalproperties/configurations/ExternalPropertiesConfig.java
@@ -8,13 +8,17 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.core.env.Environment;
 
 @Configuration
-@PropertySource({"classpath:datasource.properties","classpath:jms.properties"})
+@PropertySources({
+        @PropertySource("classpath:datasource.properties")/*,
+        @PropertySource("classpath:jms.properties")*/})
+////using the yml file for gurnish.jms properties
 public class ExternalPropertiesConfig {
-    @Autowired
-    Environment env;
+    //    @Autowired
+//    Environment env;
     @Value("${gurnish.user}")
     String user;
     @Value("${gurnish.password}")
@@ -27,21 +31,23 @@ public class ExternalPropertiesConfig {
     String jmsPassword;
     @Value("${gurnish.jms.url}")
     String jmsUrl;
+
     @Bean
-    public FakedataModel fakedataModel(){
-        FakedataModel FakedataModel =new FakedataModel();
+    public FakedataModel fakedataModel() {
+        FakedataModel FakedataModel = new FakedataModel();
         FakedataModel.setUser(user);
-//        absFakedataModel.setUser(env.getProperty("USERNAME"));
-       FakedataModel.setPassword(password);
+//        FakedataModel.setUser(env.getProperty("USERNAME"));
+        FakedataModel.setPassword(password);
         FakedataModel.setUrl(url);
         return FakedataModel;
     }
+
     @Bean  ////this doesnt work apparently because i dont know how to make multiple beans of same pojo
-    public FakeJmsdataModel fakeJmsdataModel(){
-        FakeJmsdataModel fakeJmsdataModel=new FakeJmsdataModel();
+    public FakeJmsdataModel fakeJmsdataModel() {
+        FakeJmsdataModel fakeJmsdataModel = new FakeJmsdataModel();
         fakeJmsdataModel.setUser(jmsUsername);
         fakeJmsdataModel.setPassword(jmsPassword);
-        fakeJmsdataModel.setUrl(jmsUsername);
+        fakeJmsdataModel.setUrl(jmsUrl);
         return fakeJmsdataModel;
     }
 
