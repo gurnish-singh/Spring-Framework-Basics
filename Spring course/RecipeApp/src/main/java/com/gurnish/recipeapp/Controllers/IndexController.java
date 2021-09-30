720b64ca781b0b05bdca0c12113461b63beef8ca
Gurnish <gsingh11_be17@thapar.edu>
2021-09-30 15:02:29 +0530

    added static resources closes #16

---
+++ Spring course/RecipeApp/src/main/java/com/gurnish/recipeapp/Controllers/IndexController.java
@@ -0,0 +1,15 @@
+package com.gurnish.recipeapp.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping({"/","index"})
+    public  String getIndexPage(Model model){
+        model.addAttribute("something");
+        return "index";
+
+    }
+}
---
+++ Spring course/RecipeApp/src/main/java/com/gurnish/recipeapp/RecipeAppApplication.java
@@ -0,0 +1,14 @@
+package com.gurnish.recipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeAppApplication.class, args);
+        System.out.println("hello by");
+    }
+
+}
