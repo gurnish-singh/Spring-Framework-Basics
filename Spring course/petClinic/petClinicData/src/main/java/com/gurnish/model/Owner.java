1a1b95996813d8d3866366c5969a9b5cd589b6bf
Gurnish <gsingh11_be17@thapar.edu>
2021-10-01 16:52:05 +0530

    added address and telephone number to Owner

--- Spring course/petClinic/petClinicData/src/main/java/com/gurnish/model/Owner.java
+++ Spring course/petClinic/petClinicData/src/main/java/com/gurnish/model/Owner.java
@@ -4,6 +4,8 @@ import java.util.Set;
 
 public class Owner extends Person{
     private Set<Pet> pets;
+    private String address;
+    private String telephone;
 
     public Set<Pet> getPets() {
         return pets;
@@ -11,5 +13,21 @@ public class Owner extends Person{
 
     public void setPets(Set<Pet> pets) {
         this.pets = pets;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
     }
 }
