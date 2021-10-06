2023600a6115cebcde83d845624f0e58ba6ee1c6
Gurnish <gsingh11_be17@thapar.edu>
2021-10-06 16:53:21 +0530

    added code for database initialization

---
+++ Spring course/RecipeApp/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+    INSERT INTO category (description) VALUES ('Australian');
+    INSERT INTO category (description) VALUES( 'Italian');
+    INSERT INTO category (description) VALUES('Mexican');
+    INSERT INTO category (description) VALUES ('fast food');
+    INSERT INTO unit_of_measure(uom) VALUES('Teaspoon');
+    INSERT INTO unit_of_measure(uom) VALUES('Tablespoon');
+    INSERT INTO unit_of_measure(uom) VALUES('Cup');
+    INSERT INTO unit_of_measure(uom) VALUES('Pinch');
+    INSERT INTO unit_of_measure(uom) VALUES('Ounce');
